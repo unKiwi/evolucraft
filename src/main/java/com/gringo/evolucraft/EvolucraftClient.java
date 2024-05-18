c27d5ac1cb88cb96d28d1a52241a962e75d951b1
luc <luc@nwe.li>
2024-05-18 23:17:44 +0200

    Initial commit

---
+++ src/main/java/com/gringo/evolucraft/EvolucraftClient.java
@@ -0,0 +1,10 @@
+package com.gringo.evolucraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvolucraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
